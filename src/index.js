b7768c3236fd1113262cb966ffdcc7e4eb5be70f
Nawab Khairuzzaman Mohammad Kibria <nawabkhairuzzaman@gmail.com>
2022-12-23 00:11:35 +0600

    Update index.js
    Add comment

--- src/index.js
+++ src/index.js
@@ -66,7 +66,7 @@ app.post("/persons", (req, res) => {
 
   // Create a unique id for the person
   const id = randomID()
-
+  // Throw error if age is not in the request body
   if (!age) {
     throw new Error("Age is required")
   }
@@ -79,34 +79,44 @@ app.post("/persons", (req, res) => {
 })
 
 app.get("/sum", (req, res) => {
+  
+  // Try Catch block
+  // If the statement inside try block fails
+  // Catch block will execute
   try {
+    
+    // Get x and y query params from url
     const { x, y } = req.query
-
+    // Call sum function (imported from different file)
     const sumResult = sum(x, y)
-
+    // Return response
     res.json({
       result: sumResult,
     })
+    
   } catch (error) {
+    // If the try block has any error, throw a custom error
     throw new Error("Something went wrong")
   }
 })
 
+// same as previous method
 app.get("/sub", (req, res) => {
   const { x, y } = req.query
 
-  const sumResult = sub(x, y)
+  const subResult = sub(x, y)
 
   res.json({
-    result: sumResult,
+    result: subResult,
   })
 })
 
+// request query to /sum route
 // localhost:8000/sum?x=4&y=5
 
 // get = data fetch
 // post = data create
-// put = data edit / update
+// put /patch = data edit / update
 // delete = data delete
 
 // Start the server
@@ -114,6 +124,8 @@ app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`)
 })
 
+// A test middleware that takes 3 args 
+// call next function to redirect the request to next function 
 function testMiddleware(req, res, next) {
   const query = {
     x: req.query.x,
@@ -125,6 +137,7 @@ function testMiddleware(req, res, next) {
   next()
 }
 
+// A middleware that takes 4 args , if any route has error it will execute at the end.
 function errorHandler(err, req, res, next) {
   if (err) {
     res.status(400).json({
@@ -133,4 +146,5 @@ function errorHandler(err, req, res, next) {
   }
 }
 
+// use error handler 
 app.use(errorHandler)
